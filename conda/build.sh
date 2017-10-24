8bb574994a9975e2b834bfa527eba04f12504a3f
Edward Z. Yang <ezyang@mit.edu>
2017-10-24 10:11:14 -0400

    Update comment, and the build problem is fixed now

--- conda/build.sh
+++ conda/build.sh
@@ -6,12 +6,16 @@ if [ -z "$PREFIX" ]; then
   PREFIX="$CONDA_PREFIX"
 fi
 
-# conda build will copy everything over, including build directories.
-# Don't let this pollute the build!
+# When conda-build constructs a new working copy to perform a build
+# in, it recursively copies *all* files and directories in the original
+# source directory, including any pre-existing build products (e.g.,
+# if you previously ran cmake.)  This is problematic, because if
+# a 'build' directory already exists, cmake will reuse build settings
+# rather than recompute them from scratch.  We want a fresh build, so
+# we prophylactically remove the build directory.
 rm -rf build || true
 
 mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_PREFIX_PATH="$PREFIX" -DCMAKE_BUILD_TYPE=Release $CONDA_CMAKE_ARGS ..
-# Work around https://github.com/zdevito/ATen/issues/92
-make install -j20 || make install -j20
+make install -j20
